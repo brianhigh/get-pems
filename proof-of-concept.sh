95c36cd59ad125862e2153bc31ea5931fa6bbe76
Brian High <bhigh@live.com>
2016-02-09 14:02:59 -0800

    Changed delimiter used to separate query terms stored in output filenames from underscore to dash.

--- proof-of-concept.sh
+++ proof-of-concept.sh
@@ -55,23 +55,23 @@ rm -f "$COOKIES"
 mkdir -p "$DATA"
 
 # Visit home page to get cookie
-curl -o "${DATA}/freeways_and_forms.html" \
+curl -o "${DATA}/freeways-and-forms.html" \
     "$BASEURL" -c "$COOKIES" -A "$USERAGENT" \
     --data "redirect=&username=${USERNAME}&password=${PASSWORD}&login=Login"
 
 # Visit the main detector_health page for chosen freeway to get the s_time_id
-curl -o "${DATA}/${NODE}_${CONTENT}_${FWY}_${DIR}.html" -b "$COOKIES" \
+curl -o "${DATA}/${NODE}-${CONTENT}-${FWY}-${DIR}.html" -b "$COOKIES" \
     -A "$USERAGENT" "${BASEURL}/?dnode=${NODE}&content=${CONTENT}&${LN}"
 
 # Extract the s_time_id from HTML using a regular expression
 UDATE=$(perl -wnl -e 's/name="s_time_id" value="(\d+)"/$1/g and print "$1"' \
-    "${DATA}/${NODE}_${CONTENT}_${FWY}_${DIR}.html")
+    "${DATA}/${NODE}-${CONTENT}-${FWY}-${DIR}.html")
 
 # Set query-string defaults
 DEF='eqpo=&tag=&st_cd=on&st_ch=on&st_ff=on&st_hv=on&st_ml=on&st_fr=on&st_or=on'
 
 # Get the TSV file for the detector_health for chosen freeway and date
-curl -o "${DATA}/${NODE}_${CONTENT}_${FWY}_${DIR}_${YEAR}${MONTH}${DAY}.tsv" \
+curl -o "${DATA}/${NODE}-${CONTENT}-${FWY}-${DIR}-${YEAR}${MONTH}${DAY}.tsv" \
     -b "$COOKIES" -A "$USERAGENT" \
     "${BASEURL}/?${PG}&${LN}&s_time_id=${UDATE}&s_time_id_f=${SDATE}&${DEF}"
     
