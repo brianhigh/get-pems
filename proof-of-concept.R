630baeedf03285cee0d7d07d274ff670dc6af01a
Brian High <bhigh@live.com>
2016-02-12 22:15:34 -0800

    General code clean-up and also to match updates to other R script.

--- proof-of-concept.R
+++ proof-of-concept.R
@@ -26,56 +26,26 @@ base.url <- 'http://pems.dot.ca.gov'
 user.agent <- 'Mozilla/5.0'         # https://en.wikipedia.org/wiki/User_agent
 cookies <- 'cookies.txt'            # https://en.wikipedia.org/wiki/HTTP_cookie
 
-# Configure curl to use a cookie file and a custom user agent string.
-cookies <- 'cookies.txt'
-curl <- getCurlHandle(cookiefile = cookies, cookiejar = cookies,
-                      useragent = user.agent)
-
 # Page configuration - query specification for type of report page
 form.num <- '1'
 node.name <- 'Freeway'
 content <- 'detector_health'
 export.type <- 'text'
 
-# Combine variables into a "page" (page) string
-page <- paste('report_form=', form.num, '&dnode=', node.name, '&content=', 
-              content, '&export=', export.type, sep='')
-
 # Lanes configuration - specific freeway and direction to query
 freeway <- '1'
 direction <- 'N'
-
-# Combine variables into a "lane" string
-lane <- paste('fwy=',freeway, '&dir=', direction, sep='')
 
 # Start date configuration - date for (beginning of) query (date or range)
 mm.str <- '02'
 dd.str <- '05'
 yyyy.str <- '2016'
 
-# Combine variables into a "start date" (sdate) string
-sdate <- paste(mm.str, dd.str, yyyy.str, sep='%2F')
-
-# There is no end date in this example query.
-
 # --------------------------------------------------------------------------
 
 # --------------------------------------------------------------------------
 # Functions
 # --------------------------------------------------------------------------
-
-## Function getSTimeId() finds the "s_time_id" value in an HTML document.
-# Note: You could also extract available cities and counties with this method.
-getSTimeId <- function(doc) {
-    optValues <- xpathSApply(
-        htmlParse(doc), 
-        paste('//form[@name="rpt_vars"]/table[@id="bts_report_controls"]', 
-        '/tr/td/input[@id="s_time_id"]', sep=''), 
-        function(x) xmlAttrs(x)["value"]
-        )
-    
-    return(optValues[[1]])
-}
 
 ## Function getFreeways() finds "freeway" choices in HTML select option tags.
 # Note: You could also extract available cities and counties with this method.
@@ -99,11 +69,18 @@ getFreeways <- function(doc) {
 # Create the data folder if needed.
 dir.create(file.path(data.folder), showWarnings = FALSE, recursive = TRUE)
 
+# Combine variables into login "form data" (formdata) string
+formdata <- paste('redirect=&username=', username, '&password=', password, 
+                  '&login=Login', sep='')
+
+# Configure curl to use a cookie file and a custom user agent string.
+cookies <- 'cookies.txt'
+curl <- getCurlHandle(cookiefile = cookies, cookiejar = cookies,
+                      useragent = user.agent)
+
 # Load homepage, get a cookie, and parse output for a dataframe of freeways.
 # From this dataframe, we can look-up the freeway number and lane directions.
 # We can loop-through this dataframe (or a subset) to process many freeways.
-formdata <- paste('redirect=&username=', username, '&password=', password, 
-                  '&login=Login', sep='')
 r = dynCurlReader()
 res <- curlPerform(postfields = formdata, url = base.url, curl=curl,
                    post = 1L, writefunction = r$update)
@@ -116,11 +93,21 @@ s.time.id <- as.character(as.integer(
     as.POSIXct(paste(yyyy.str, mm.str, dd.str, sep='-'), 
                origin="1970-01-01", tz = "GMT")))
 
+# Combine variables into a "page" (page) string
+page <- paste('report_form=', form.num, '&dnode=', node.name, '&content=', 
+              content, '&export=', export.type, sep='')
+
+# Combine variables into a "lane" string
+lane <- paste('fwy=',freeway, '&dir=', direction, sep='')
+
+# Combine variables into a "start date" (sdate) string
+sdate <- paste(mm.str, dd.str, yyyy.str, sep='%2F')
+
 # Get the TSV file for the detector_health for chosen freeway and date
-url <- paste(base.url, '/?', page, '&', lane, '&s_time_id=', s.time.id, 
+r.url <- paste(base.url, '/?', page, '&', lane, '&s_time_id=', s.time.id, 
              '&s_time_id_f=', sdate, sep='')
 r = dynCurlReader()
-result.string <- getURL(url = url, curl = curl)
+result.string <- getURL(url = r.url, curl = curl)
 writeLines(result.string, 
            paste(data.folder, '/', node.name, '-', content, '-', freeway, '-', 
                  direction, '-', yyyy.str, mm.str, dd.str, '.tsv', sep=""))
